12e0e6ff52ecb1e8a5c656c6720fba6478dc61af
NormanZ <1275133384@qq.com>
2023-09-21 17:02:01 +0800

    update example

--- example.py
+++ example.py
@@ -17,7 +17,8 @@ data  = torch.cat([
 ])
 plt.hist(data.numpy(), bins=100)
 plt.title("Ground Truth Data distribution")
-plt.show()
+# plt.show()
+plt.savefig("./assert/Ground_Truth_Distribution.png")
 
 # Create dataset and dataloader
 dataset = TensorDataset(data)
@@ -70,3 +71,57 @@ class ConsistencyFunction(nn.Module):
         inputs = torch.cat([x, t.unsqueeze(-1)], dim=-1)
         outputs = self.mlp(inputs)
         return ((T - t) / (T - EPSILON)).unsqueeze(-1) * x + ((t - EPSILON) / (T - EPSILON)).unsqueeze(-1) * outputs
+    
+
+# Score function (Denoiser actually...) training
+denoiser = Denoiser()
+denoiser_optimizer = torch.optim.Adam(denoiser.parameters(), lr=1e-3)
+denoiser.train()
+
+# Discretize time steps. From EDM Karras et al. It is dense in the beginning and sparse in the end
+n_steps_straining = 1000
+t_steps = [(EPSILON ** (1 / 7) + (i / (n_steps_straining - 1)) * (T ** (1 / 7) - EPSILON ** (1 / 7))) ** 7 for i in range(0, n_steps_straining)]
+
+N_EPOCHS = 100
+
+with trange(N_EPOCHS) as pbar:
+    for epoch in range(N_EPOCHS):
+        tot_loss = 0.0
+        for x, in dataloader:
+            batch_size = x.shape[0]
+            
+            # Sample time
+            t = torch.tensor(random.choices(t_steps, k=batch_size))
+            
+            # Sample random Gaussian noise
+            z = torch.randn(batch_size, 1)
+
+            # Compute loss and update score function xt|x ~ N(xt; x, t^2 I)
+            loss = F.mse_loss(denoiser(x + t.unsqueeze(-1) * z, t), x)
+            
+            loss.backward()
+            denoiser_optimizer.step()
+            denoiser_optimizer.zero_grad()
+
+            tot_loss += loss.item()
+        pbar.set_postfix(loss=tot_loss / len(dataloader))
+        pbar.update()
+
+
+@torch.no_grad()
+def deterministic_sampling(xT: torch.Tensor, n_steps: int):
+    "Deterministic sampling using Heun's 2nd order method"
+    t_steps = [(EPSILON ** (1 / 7) + (j / (n_steps - 1)) * (T ** (1 / 7) - EPSILON ** (1 / 7))) ** 7 for j in range(n_steps - 1, 0, -1)]
+    batch_size = xT.shape[0]
+    xi = xT
+    trajectory = [xT]
+    for i in range(0, len(t_steps) - 1):
+        d = -t_steps[i] * score_function(denoiser, xi, torch.ones(batch_size) * t_steps[i])
+        xi_1 = xi + (t_steps[i + 1] - t_steps[i]) * d
+        d_ = -t_steps[i + 1] * score_function(denoiser, xi_1, torch.ones(batch_size) * t_steps[i + 1])
+        xi_1 = xi + (t_steps[i + 1] - t_steps[i]) / 2 * (d + d_)
+
+        xi = xi_1
+        trajectory.append(xi)
+        
+    return xi, (t_steps, trajectory)
